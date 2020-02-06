bb8364276d34a5f20cc21ca4c83e2399d3fb3363
Erdal <erdalguzel@users.noreply.github.com>
2020-02-06 15:48:27 +0300

    Extensions are modified

--- Nerde Yesem/Nerde Yesem/Extensions/AppDelegate+Additions.swift
+++ Nerde Yesem/Nerde Yesem/Extensions/AppDelegate+Additions.swift
@@ -3,15 +3,15 @@ import CoreLocation
 import LocalAuthentication
 import UIKit
 
-// MARK: Touch ID extension
+// MARK: - LocalAuthentication extension
 extension AppDelegate {
 	func authenticateUser() {
 		let context = LAContext()
 		var error: NSError?
 		
-		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+		if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
 			let reason = "Authenticate with Touch ID"
-			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
+			context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason, reply:
 				{(success, error) in
 					if success {
 						self.alertUser("You have successfully logged in!", "Touch ID Authentication Succeeded")
@@ -34,11 +34,27 @@ extension AppDelegate {
 			self.window?.rootViewController?.present(alert, animated: true, completion: nil)
 		}
 	}
-	
+}
+
+// MARK: - Core Location Delegate extension
+extension AppDelegate {
 	func setupLocationManager() {
 		locationManager.delegate = self
 		locationManager.requestWhenInUseAuthorization()
 		locationManager.desiredAccuracy = kCLLocationAccuracyBest
 		locationManager.startUpdatingLocation()
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		switch status {
+		case .denied:
+		self.alertUser("Please turn on location services to get the best experience", "Turn on location services")
+		case .notDetermined:
+		self.alertUser("For some reason we cannot determine location usage. App may not function normally in this state.", "Location usage not determined")
+		case .restricted:
+		self.alertUser("Location usage is restricted right not. App may not function normally in this state.", "Location usage is restricted")
+		default:
+		break
+		}
 	}
 }
